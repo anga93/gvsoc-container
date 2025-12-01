bbaa4c90e51e2b13fa44a85efd89d23ce30303fa
unknown <472675196@qq.com>
2025-12-01 15:44:52 +0800

    inial files to build container with docker for GVSOC environment on X86

--- push-x86.sh
+++ push-x86.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker image tag mcai/gvsoc-container-x86:latest mcai/gvsoc-container-x86:latest 
-docker push mcai/gvsoc-container-x86:latest
+docker image tag mcai/gvsoc-container-x86:latest caimaoyuan/gvsoc-container-x86:latest
+docker push caimaoyuan/gvsoc-container-x86:latest
